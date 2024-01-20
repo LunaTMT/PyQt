3eaf3aeeba42a595b93db2ddb973d3e23981e9fa
LunaTMT <taylorthreader@gmail.com>
2024-01-20 19:29:22 +0000

    folders added and listing 2.2

---
+++ 2.2/labels.py
@@ -0,0 +1,37 @@
+import sys
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel
+from PyQt5.QtGui import QPixmap
+
+class HelloWorldWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initializeUI()
+
+    def initializeUI(self):
+
+        self.setGeometry(100, 100, 250, 250)
+        self.setWindowTitle('Qlabel Example')
+        self.displayLabels()
+
+        self.show()
+
+    def displayLabels(self):
+        text = QLabel(self)
+        text.setText("Hello")
+        text.move(105, 15)
+
+        image = "images/world.png"
+        try:
+            with open(image):
+                world_image = QLabel(self)
+                pixmap = QPixmap(image)
+                world_image.setPixmap(pixmap)
+                world_image.move(25, 40)
+        except FileNotFoundError:
+            print("Image not found")
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = HelloWorldWindow()
+    sys.exit(app.exec_())

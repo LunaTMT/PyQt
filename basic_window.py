34be80cedc6591a13dded1dd8da5ea54719d4d7a
LunaTMT <taylorthreader@gmail.com>
2024-01-20 18:55:48 +0000

    problem with super

--- basic_window.py
+++ basic_window.py
@@ -5,14 +5,13 @@ from PyQt5.QtWidgets import QApplication, QWidget
 class EmptyWindow(QWidget):
 
     def __init__(self):
-        super.__init__()
+        super().__init__()
         self.initializeUI()
 
     def initializeUI(self):
         self.setGeometry(100, 100, 400, 300)
         self.setWindowTitle('Empty Window in PyQt')
         self.show()
-
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
